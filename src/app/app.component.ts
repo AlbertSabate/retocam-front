8df9ac646b2dd6e297306f9485e7451276ce676a
Albert Sabate Martinez <albertsabatemartinez@MacBook-Pro-de-Albert.local>
2017-01-18 22:46:52 +0100

    Add assets && angularjs 2 load ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'RETOCA\'Mmmm';
+  subtitle = 'Festa de la Percussió de Les Corts';
 }
